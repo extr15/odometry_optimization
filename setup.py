f1390b29b42bad3bf5536e8be52b9ecb9dd2bc59
Miquel Massot <miquel.massot@uib.cat>
2014-01-14 16:22:18 +0100

    fixed catkinization

--- setup.py
+++ setup.py
@@ -6,8 +6,8 @@ from catkin_pkg.python_setup import generate_distutils_setup
 d = generate_distutils_setup(
    ##  don't do this unless you want a globally visible script
    # scripts=['bin/myscript'], 
-   packages=['PYTHON_PACKAGE_NAME'],
-   package_dir={'': 'src'}
+   packages=['odometry_optimization'],
+   package_dir={'': 'scripts'}
 )
 
 setup(**d)
